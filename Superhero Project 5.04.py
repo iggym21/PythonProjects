f1857c315d40bb360fb4cf17acea5508ade8ac18
iggym21 <ignatiusmartin6@gmail.com>
2020-11-26 21:26:03 -0500

    initial commit

---
+++ Superhero Project 5.04.py
@@ -0,0 +1,20 @@
+class Superhero:
+    # Superhero class represents the facts related to a superhero.
+    def __init__(self, name = "", strengthPts = 0 , motto = "", superpower = ""):
+        # Create a new Superhero with a name and other attributes
+        self.name = "Phoenix" 
+        self.strengthPts = 1232
+        self.motto = "Run it back"
+        self.superpower = "Can control and create fire with his hands"
+def addStrengthPts(self, points = 0):
+        self = Superhero("Phoenix" , 1232 , "Run it Back" , "Can control and create fire with his hands")
+        # Adds points to the superhero's strength.
+        self.strengthPts = self.strengthPts + points
+def main():
+        self = Superhero("Phoenix" , 1232 , "Run it Back" , "Can control and create fire with his hands")
+        addStrengthPts(0)
+        print("Name: " +  (self.name))
+        print("Strength Points: " + str(self.strengthPts))
+        print("Motto: " + self.motto)
+        print("Superpower: " + self.superpower)
+main()
